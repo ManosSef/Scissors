6b68da7ccf2bcb5f4384ba49e5ac585dc302cc13
ManosSef <mseferlis05@gmail.com>
2025-10-22 14:45:50 +0300

    Fix IllegalArgumentException thrown by 8<roll

--- src/main/java/me/manossef/scissors/commands/RollCommand.java
+++ src/main/java/me/manossef/scissors/commands/RollCommand.java
@@ -2,11 +2,16 @@ package me.manossef.scissors.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import me.manossef.scissors.ChatCommandSource;
 import me.manossef.scissors.Commands;
 import me.manossef.scissors.Scissors;
 
 public class RollCommand {
+
+    private static final SimpleCommandExceptionType MAX_LESS_THAN_1 = new SimpleCommandExceptionType(() -> "The maximum value cannot be less than 1");
+    private static final SimpleCommandExceptionType MAX_LESS_THAN_MIN = new SimpleCommandExceptionType(() -> "The maximum value cannot be less than the minimum value");
 
     public static void register(CommandDispatcher<ChatCommandSource> dispatcher) {
 
@@ -23,14 +28,16 @@ public class RollCommand {
 
     }
 
-    private static int roll(ChatCommandSource source, int max) {
+    private static int roll(ChatCommandSource source, int max) throws CommandSyntaxException {
 
+        if(max < 1) throw MAX_LESS_THAN_1.create();
         return roll(source, 1, max);
 
     }
 
-    private static int roll(ChatCommandSource source, int min, int max) {
+    private static int roll(ChatCommandSource source, int min, int max) throws CommandSyntaxException {
 
+        if(max < min) throw MAX_LESS_THAN_MIN.create();
         int random = Scissors.RANDOM.nextInt(min, max + 1);
         source.sendSuccess("You rolled " + random);
         return random;
