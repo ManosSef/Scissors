6386746c6e6113538cb02d11b106166830eb31ee
ManosSef <mseferlis05@gmail.com>
2026-02-15 03:48:15 +0200

    Move responses to separate class and refactor code for sending responses

--- src/main/java/me/manossef/scissors/listeners/MessageListeners.java
+++ src/main/java/me/manossef/scissors/listeners/MessageListeners.java
@@ -14,52 +14,6 @@ import java.util.List;
 
 public class MessageListeners extends ListenerAdapter {
 
-    private static final List<String> GPPCT_RESPONSES = List.of(
-        "As part of the Great Purge of Pointless Counting Threads (GPPCT), this channel has been closed due to the following criteria: **Not a counting channel.**",
-        "RAHHH! No counting!",
-        "I don't even get where's the fun in counting channels",
-        "Be careful not to stationary zero too much or you might end up with zero brain cells",
-        "Whoever invented the term \"stationary zeroing\" anyway?",
-        "I hope this is hardcore counting so you are prevented from doing that again",
-        "Reminder that this is not a counting channel",
-        "What channel is this again?",
-        "WHAT DID I SAY ABOUT COUNTING?",
-        "HEY STOP IT",
-        "I HOPE YOU GET BANNED FOR SPAMMING",
-        "What's the punishment for spamming again?",
-        "Did I miss this channel being renamed to #arbitrary-counting?",
-        "BRO WHAT'S WITH YOU AND NUMBERS",
-        "I'm made of numbers and I don't like them",
-        "Right, now you have to be doing it on purpose",
-        "Are you kidding me",
-        "I'm gonna call the GPPCT police",
-        "You know what, I guess you can keep doing it, it's not like I can stop you",
-        "This isn't #counting, is it?",
-        "Oh my goodness! Is that... a... a... *number*?",
-        "Is that your age?",
-        "\uD83D\uDE21",
-        "Don't you have anything better to do?",
-        "NO COUNTING NO COUNTING NO COUNTING NO COUNTING",
-        "YOU HAVE TO BE RAGEBAITING RIGHT NOW",
-        "Ah yes, counting... I used to like it, until it corrupted me",
-        "Trust me, rock paper scissors is more fun than this",
-        "?warn %s Spam",
-        "Would you like me to summon the admins?",
-        "Are you testing my nerves or something?",
-        "A number?! At this time of year? At this time of day? In this part of the country? ~~Localized entirely within your kitchen?!~~",
-        "How did my creator not go crazy from having to deal with this",
-        "You're the reason I crash this often",
-        "I'm not a fan of your schadenfreude",
-        "Do you really want to test me? *snips*",
-        "My blades are very sharp. Consider this a threat",
-        "You're probably one of those school nerds that have no friends"
-    );
-    private static final List<String> GPPCT_BRAINROT_RESPONSES = List.of(
-        "sIx SeVeN",
-        "GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD",
-        "HOW DID PEOPLE MAKE A MEME OUT OF JUST TWO NUMBERS???"
-    );
-
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
@@ -70,28 +24,14 @@ public class MessageListeners extends ListenerAdapter {
         if(content.matches("^[0-9]+$")) {
 
             if(Scissors.RANDOM.nextInt(10) == 0 && channel.canTalk()
-                && !(channel.getName().toLowerCase().contains("counting") || channel.getName().toLowerCase().contains("spam") || (message.getCategory() != null && message.getCategory().getName().toLowerCase().contains("counting")))) {
-
-                if(content.equals("67"))
-                    channel.sendMessage(GPPCT_BRAINROT_RESPONSES.get(Scissors.RANDOM.nextInt(GPPCT_BRAINROT_RESPONSES.size())).formatted(message.getAuthor().getAsMention()))
-                        .setMessageReference(message)
-                        .mentionRepliedUser(false)
-                        .setAllowedMentions(Collections.emptyList())
-                        .queue();
-                else
-                    channel.sendMessage(GPPCT_RESPONSES.get(Scissors.RANDOM.nextInt(GPPCT_RESPONSES.size())).formatted(message.getAuthor().getAsMention()))
-                        .setMessageReference(message)
-                        .mentionRepliedUser(false)
-                        .setAllowedMentions(Collections.emptyList())
-                        .queue();
-
-            }
+                && !(channel.getName().toLowerCase().contains("counting") || channel.getName().toLowerCase().contains("spam") || (message.getCategory() != null && message.getCategory().getName().toLowerCase().contains("counting"))))
+                replyWithRandomMessage(message, content.equals("67") ? Messages.GPPCT_BRAINROT_RESPONSES : Messages.GPPCT_RESPONSES);
 
         }
         if(content.toLowerCase().contains("scissors")) {
 
             if(Scissors.RANDOM.nextInt(5) == 0 && channel.canTalk())
-                channel.sendMessage("RAHHH!").setMessageReference(message).mentionRepliedUser(false).queue();
+                replyWithRandomMessage(message, Messages.SCISSORS_RESPONSES);
 
         }
         if(content.toLowerCase().contains("paper"))
@@ -106,6 +46,16 @@ public class MessageListeners extends ListenerAdapter {
         if(!event.getEmoji().getName().equals("\uD83D\uDDD1\uFE0F")) return;
         if(event.getUserIdLong() != SharedConstants.MY_USER_ID) return;
         event.retrieveMessage().onSuccess(message -> message.delete().queue()).queue();
+
+    }
+
+    private void replyWithRandomMessage(Message message, List<String> possibleMessages) {
+
+        message.getChannel().sendMessage(possibleMessages.get(Scissors.RANDOM.nextInt(possibleMessages.size())).formatted(message.getAuthor().getAsMention()))
+            .setMessageReference(message)
+            .mentionRepliedUser(false)
+            .setAllowedMentions(Collections.emptyList())
+            .queue();
 
     }
 
---
+++ src/main/java/me/manossef/scissors/listeners/Messages.java
@@ -0,0 +1,57 @@
+package me.manossef.scissors.listeners;
+
+import java.util.List;
+
+public class Messages {
+
+    static final List<String> GPPCT_RESPONSES = List.of(
+        "As part of the Great Purge of Pointless Counting Threads (GPPCT), this channel has been closed due to the following criteria: **Not a counting channel.**",
+        "RAHHH! No counting!",
+        "I don't even get where's the fun in counting channels",
+        "Be careful not to stationary zero too much or you might end up with zero brain cells",
+        "Whoever invented the term \"stationary zeroing\" anyway?",
+        "I hope this is hardcore counting so you are prevented from doing that again",
+        "Reminder that this is not a counting channel",
+        "What channel is this again?",
+        "WHAT DID I SAY ABOUT COUNTING?",
+        "HEY STOP IT",
+        "I HOPE YOU GET BANNED FOR SPAMMING",
+        "What's the punishment for spamming again?",
+        "Did I miss this channel being renamed to #arbitrary-counting?",
+        "BRO WHAT'S WITH YOU AND NUMBERS",
+        "I'm made of numbers and I don't like them",
+        "Right, now you have to be doing it on purpose",
+        "Are you kidding me",
+        "I'm gonna call the GPPCT police",
+        "You know what, I guess you can keep doing it, it's not like I can stop you",
+        "This isn't #counting, is it?",
+        "Oh my goodness! Is that... a... a... *number*?",
+        "Is that your age?",
+        "\uD83D\uDE21",
+        "Don't you have anything better to do?",
+        "NO COUNTING NO COUNTING NO COUNTING NO COUNTING",
+        "YOU HAVE TO BE RAGEBAITING RIGHT NOW",
+        "Ah yes, counting... I used to like it, until it corrupted me",
+        "Trust me, rock paper scissors is more fun than this",
+        "?warn %s Spam",
+        "Would you like me to summon the admins?",
+        "Are you testing my nerves or something?",
+        "A number?! At this time of year? At this time of day? In this part of the country? ~~Localized entirely within your kitchen?!~~",
+        "How did my creator not go crazy from having to deal with this",
+        "You're the reason I crash this often",
+        "I'm not a fan of your schadenfreude",
+        "Do you really want to test me? *snips*",
+        "My blades are very sharp. Consider this a threat",
+        "You're probably one of those school nerds that have no friends",
+        "This feels worse than being beaten up by a rock"
+    );
+    static final List<String> GPPCT_BRAINROT_RESPONSES = List.of(
+        "sIx SeVeN",
+        "GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD",
+        "HOW DID PEOPLE MAKE A MEME OUT OF JUST TWO NUMBERS???"
+    );
+    static final List<String> SCISSORS_RESPONSES = List.of(
+        "RAHHH!"
+    );
+
+}
