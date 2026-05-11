94dcc8ce0d07d65d3fa526c07c903f0c54935a61
ManosSef <mseferlis05@gmail.com>
2026-05-12 01:58:13 +0300

    Log status when joining or leaving a guild

--- src/main/java/me/manossef/scissors/listeners/Startup.java
+++ src/main/java/me/manossef/scissors/listeners/Startup.java
@@ -1,6 +1,9 @@
 package me.manossef.scissors.listeners;
 
 import me.manossef.scissors.DevGuild;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -8,5 +11,17 @@ public class Startup extends ListenerAdapter {
     @Override
     public void onReady(ReadyEvent event) {
         DevGuild.logStatus("Hello, world! I sure hope no rocks get in my way today!");
+    }
+
+    @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        Guild guild = event.getGuild();
+        DevGuild.logStatus("Joined server " + guild.getName() + " (ID: " + guild.getId() + ")");
+    }
+
+    @Override
+    public void onGuildLeave(GuildLeaveEvent event) {
+        Guild guild = event.getGuild();
+        DevGuild.logStatus("Left server " + guild.getName() + " (ID: " + guild.getId() + ")");
     }
 }
