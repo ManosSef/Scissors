d076aadb93f47bbacd9ddc98cfb6b6fe89cd92c0
ManosSef <mseferlis05@gmail.com>
2026-07-22 21:17:45 +0300

    Add a way to roll long

--- src/main/java/me/manossef/scissors/commands/RollCommand.java
+++ src/main/java/me/manossef/scissors/commands/RollCommand.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.LiteralMessage;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
 import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.LongArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import me.manossef.scissors.ChatCommandSource;
@@ -18,7 +19,7 @@ import static net.dv8tion.jda.api.utils.MarkdownUtil.monospace;
 public class RollCommand {
     private static final SimpleCommandExceptionType MAX_LESS_THAN_1 = new SimpleCommandExceptionType(new LiteralMessage("The maximum value cannot be less than 1"));
     private static final SimpleCommandExceptionType MAX_LESS_THAN_MIN = new SimpleCommandExceptionType(new LiteralMessage("The maximum value cannot be less than the minimum value"));
-    private static final SimpleCommandExceptionType MAX_FLOAT_LESS_THAN_0 = new SimpleCommandExceptionType(new LiteralMessage("The maximum value must be greater 0"));
+    private static final SimpleCommandExceptionType MAX_FLOAT_LESS_THAN_0 = new SimpleCommandExceptionType(new LiteralMessage("The maximum value must be greater than 0"));
     private static final SimpleCommandExceptionType MAX_FLOAT_LESS_THAN_MIN = new SimpleCommandExceptionType(new LiteralMessage("The maximum value must be greater than the minimum value"));
 
     public static void register(CommandDispatcher<ChatCommandSource> dispatcher) {
@@ -40,6 +41,16 @@ public class RollCommand {
                     .executes(context -> roll(context.getSource(), context.getArgument("minFloat", Float.class), context.getArgument("maxFloat", Float.class)))
                 )
             )
+            .then(Commands.literal("long")
+                .then(Commands.argument("maxLong", LongArgumentType.longArg())
+                    .executes(context -> roll(context.getSource(), context.getArgument("maxLong", Long.class)))
+                )
+                .then(Commands.argument("minLong", LongArgumentType.longArg())
+                    .then(Commands.argument("maxLong", LongArgumentType.longArg())
+                        .executes(context -> roll(context.getSource(), context.getArgument("minLong", Long.class), context.getArgument("maxLong", Long.class)))
+                    )
+                )
+            )
             .then(Commands.literal("double")
                 .then(Commands.argument("maxDouble", DoubleArgumentType.doubleArg())
                     .executes(context -> roll(context.getSource(), context.getArgument("maxDouble", Double.class)))
@@ -56,8 +67,10 @@ public class RollCommand {
                 Rolls a random number from a specific range.
                 
                 Here are the available syntaxes for this command:
-                - %3$s: If %2$s is an integer, rolls an integer from 1 to %2$s (inclusive). Otherwise, rolls a decimal number from 0 to %2$s with at most 9 decimal digits.
-                - %4$s: Rolls a number from %1$s to %2$s. If both %1$s and %2$s are integers, only rolls integers. Otherwise, rolls any decimal number in the range with at most 9 decimal digits.
+                - %3$s: If %2$s is an integer (from 1 to %9$s), rolls an integer from 1 to %2$s (inclusive). Otherwise, rolls a decimal number from 0 to %2$s with at most 9 decimal digits.
+                - %4$s: Rolls a number from %1$s to %2$s. If both %1$s and %2$s are integers (from %10$s to %9$s), only rolls integers. Otherwise, rolls any decimal number in the range with at most 9 decimal digits.
+                - %7$s: Rolls a long integer from 1 to %2$s (inclusive). %2$s must be at most %11$s.
+                - %8$s: Rolls a long integer from %1$s to %2$s. %1$s and %2$s must be between %12$s and %11$s (inclusive).
                 - %5$s: Rolls a decimal number from 0 to %2$s with at most 17 decimal digits.
                 - %6$s: Rolls a decimal number from %1$s to %2$s with at most 17 decimal digits.""",
             monospace("<min>"),
@@ -65,7 +78,13 @@ public class RollCommand {
             monospace(SharedConstants.COMMAND_PREFIX + baseLiteral + " <max>"),
             monospace(SharedConstants.COMMAND_PREFIX + baseLiteral + " <min> <max>"),
             monospace(SharedConstants.COMMAND_PREFIX + baseLiteral + " double <max>"),
-            monospace(SharedConstants.COMMAND_PREFIX + baseLiteral + " double <min> <max>")));
+            monospace(SharedConstants.COMMAND_PREFIX + baseLiteral + " double <min> <max>"),
+            monospace(SharedConstants.COMMAND_PREFIX + baseLiteral + " long <max>"),
+            monospace(SharedConstants.COMMAND_PREFIX + baseLiteral + " long <min> <max>"),
+            Integer.MAX_VALUE,
+            Integer.MIN_VALUE,
+            Long.MAX_VALUE,
+            Long.MIN_VALUE));
     }
 
     private static int roll(ChatCommandSource source, int max) throws CommandSyntaxException {
@@ -78,6 +97,18 @@ public class RollCommand {
         int random = Scissors.RANDOM.nextInt(min, max + 1);
         source.sendSuccess("You rolled " + bold(String.valueOf(random)));
         return random;
+    }
+
+    private static int roll(ChatCommandSource source, long max) throws CommandSyntaxException {
+        if(max < 1) throw MAX_LESS_THAN_1.create();
+        return roll(source, 1, max);
+    }
+
+    private static int roll(ChatCommandSource source, long min, long max) throws CommandSyntaxException {
+        if(max < min) throw MAX_LESS_THAN_MIN.create();
+        long random = Scissors.RANDOM.nextLong(min, max + 1);
+        source.sendSuccess("You rolled " + bold(String.valueOf(random)));
+        return (int) random;
     }
 
     private static int roll(ChatCommandSource source, float max) throws CommandSyntaxException {
