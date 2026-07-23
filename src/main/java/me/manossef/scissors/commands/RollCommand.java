65567aa0a59a70fe5dd847e324f495df1e750581
ManosSef <mseferlis05@gmail.com>
2026-07-23 23:52:42 +0300

    Eliminate confusion from 8<roll's 8<help

--- src/main/java/me/manossef/scissors/commands/RollCommand.java
+++ src/main/java/me/manossef/scissors/commands/RollCommand.java
@@ -68,8 +68,8 @@ public class RollCommand {
                 Here are the available syntaxes for this command:
                 - %3$s: If %2$s is an integer (from 1 to %9$s), rolls an integer from 1 to %2$s (inclusive). Otherwise, rolls a decimal number from 0 to %2$s with at most 9 decimal digits.
                 - %4$s: Rolls a number from %1$s to %2$s. If both %1$s and %2$s are integers (from %10$s to %9$s), only rolls integers. Otherwise, rolls any decimal number in the range with at most 9 decimal digits.
-                - %7$s: Rolls a long integer from 1 to %2$s (inclusive). %2$s must be at most %11$s.
-                - %8$s: Rolls a long integer from %1$s to %2$s. %1$s and %2$s must be between %12$s and %11$s (inclusive).
+                - %7$s: Rolls an integer from 1 to %2$s (inclusive). %2$s must be at most %11$s.
+                - %8$s: Rolls an integer from %1$s to %2$s. %1$s and %2$s must be between %12$s and %11$s (inclusive).
                 - %5$s: Rolls a decimal number from 0 to %2$s with at most 17 decimal digits.
                 - %6$s: Rolls a decimal number from %1$s to %2$s with at most 17 decimal digits.""",
             monospace("<min>"),
