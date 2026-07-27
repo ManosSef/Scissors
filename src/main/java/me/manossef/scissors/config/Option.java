18030dc9d8a108e7026638a6b95248b9a59597d2
ManosSef <mseferlis05@gmail.com>
2026-07-27 21:15:20 +0300

    Add default value to options

--- src/main/java/me/manossef/scissors/config/Option.java
+++ src/main/java/me/manossef/scissors/config/Option.java
@@ -4,46 +4,52 @@ import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 
-public class Option<T> {
+public final class Option<T> {
     private final String name;
     private final Class<T> type;
     private final ArgumentType<T> argumentType;
+    private final T defaultValue;
 
-    private Option(String name, Class<T> type, ArgumentType<T> argumentType) {
+    private Option(String name, Class<T> type, ArgumentType<T> argumentType, T defaultValue) {
         this.name = name;
         this.type = type;
         this.argumentType = argumentType;
+        this.defaultValue = defaultValue;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public Class<T> getType() {
-        return type;
+        return this.type;
     }
 
     public ArgumentType<T> getArgumentType() {
-        return argumentType;
+        return this.argumentType;
     }
 
-    static Option<Boolean> bool(String name) {
-        return new Option<>(name, Boolean.class, BoolArgumentType.bool());
+    public T getDefaultValue() {
+        return this.defaultValue;
     }
 
-    static Option<Integer> integer(String name) {
-        return integer(name, IntegerArgumentType.integer());
+    static Option<Boolean> bool(String name, boolean defaultValue) {
+        return new Option<>(name, Boolean.class, BoolArgumentType.bool(), defaultValue);
     }
 
-    static Option<Integer> integer(String name, int min) {
-        return integer(name, IntegerArgumentType.integer(min));
+    static Option<Integer> integer(String name, int defaultValue) {
+        return integer(name, IntegerArgumentType.integer(), defaultValue);
     }
 
-    static Option<Integer> integer(String name, int min, int max) {
-        return integer(name, IntegerArgumentType.integer(min, max));
+    static Option<Integer> integer(String name, int defaultValue, int min) {
+        return integer(name, IntegerArgumentType.integer(min), defaultValue);
     }
 
-    private static Option<Integer> integer(String name, ArgumentType<Integer> argumentType) {
-        return new Option<>(name, Integer.class, argumentType);
+    static Option<Integer> integer(String name, int defaultValue, int min, int max) {
+        return integer(name, IntegerArgumentType.integer(min, max), defaultValue);
+    }
+
+    private static Option<Integer> integer(String name, ArgumentType<Integer> argumentType, int defaultValue) {
+        return new Option<>(name, Integer.class, argumentType, defaultValue);
     }
 }
