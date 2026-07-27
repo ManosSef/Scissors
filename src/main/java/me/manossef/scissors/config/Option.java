d3c7f395c98bb7a5f66dcfb748bd38c5541224eb
ManosSef <mseferlis05@gmail.com>
2026-07-27 21:12:51 +0300

    Get int options from factory methods instead

--- src/main/java/me/manossef/scissors/config/Option.java
+++ src/main/java/me/manossef/scissors/config/Option.java
@@ -1,13 +1,15 @@
 package me.manossef.scissors.config;
 
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 
 public class Option<T> {
     private final String name;
     private final Class<T> type;
     private final ArgumentType<T> argumentType;
 
-    Option(String name, Class<T> type, ArgumentType<T> argumentType) {
+    private Option(String name, Class<T> type, ArgumentType<T> argumentType) {
         this.name = name;
         this.type = type;
         this.argumentType = argumentType;
@@ -23,5 +25,25 @@ public class Option<T> {
 
     public ArgumentType<T> getArgumentType() {
         return argumentType;
+    }
+
+    static Option<Boolean> bool(String name) {
+        return new Option<>(name, Boolean.class, BoolArgumentType.bool());
+    }
+
+    static Option<Integer> integer(String name) {
+        return integer(name, IntegerArgumentType.integer());
+    }
+
+    static Option<Integer> integer(String name, int min) {
+        return integer(name, IntegerArgumentType.integer(min));
+    }
+
+    static Option<Integer> integer(String name, int min, int max) {
+        return integer(name, IntegerArgumentType.integer(min, max));
+    }
+
+    private static Option<Integer> integer(String name, ArgumentType<Integer> argumentType) {
+        return new Option<>(name, Integer.class, argumentType);
     }
 }
