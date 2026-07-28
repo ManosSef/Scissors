56a954b83d067d4d8d3c5386c16da467c08960b6
ManosSef <mseferlis05@gmail.com>
2026-07-29 01:29:19 +0300

    Add settings class

---
+++ src/main/java/me/manossef/scissors/config/Settings.java
@@ -0,0 +1,35 @@
+package me.manossef.scissors.config;
+
+import java.util.*;
+
+public class Settings {
+    private final Map<Option<?>, OptionValue<?>> values;
+
+    Settings() {
+        this.values = new TreeMap<>((o1, o2) -> Comparator.<String>naturalOrder().compare(o1.getName(), o2.getName()));
+    }
+
+    public <T> Optional<T> get(Option<T> option) {
+        Objects.requireNonNull(option, "Option cannot be null");
+        OptionValue<?> value = values.get(option);
+        if(value == null) return Optional.empty();
+        return Optional.ofNullable(option.getType().cast(value.value()));
+    }
+
+    public <T> boolean set(Option<T> option, T value) {
+        Objects.requireNonNull(option, "Option cannot be null");
+        Objects.requireNonNull(value, "Value cannot be null");
+        OptionValue<T> optionValue = new OptionValue<>(option, value);
+        return !optionValue.equals(values.put(option, optionValue));
+    }
+
+    public boolean resetToDefault() {
+        if(values.isEmpty()) return false;
+        values.clear();
+        return true;
+    }
+
+    public String toString() {
+        return "Settings[values=" + values.values() + "]";
+    }
+}
