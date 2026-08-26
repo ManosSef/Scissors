e2da7b3718804d5260f87993846fc4547f4db891
ManosSef <mseferlis05@gmail.com>
2026-08-26 18:45:56 +0300

    Show string argument length limit in the error

--- src/main/java/me/manossef/scissors/arguments/LengthLimitedStringArgumentType.java
+++ src/main/java/me/manossef/scissors/arguments/LengthLimitedStringArgumentType.java
@@ -4,10 +4,10 @@ import com.mojang.brigadier.LiteralMessage;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 
 public class LengthLimitedStringArgumentType implements ArgumentType<String> {
-    private static final SimpleCommandExceptionType TOO_LONG = new SimpleCommandExceptionType(new LiteralMessage("The given string is too long"));
+    private static final DynamicCommandExceptionType TOO_LONG = new DynamicCommandExceptionType(expected -> new LiteralMessage("Expected a string not longer than " + expected + " characters"));
 
     private final int maxLength;
 
@@ -23,9 +23,10 @@ public class LengthLimitedStringArgumentType implements ArgumentType<String> {
     public String parse(StringReader reader) throws CommandSyntaxException {
         int position = reader.getCursor();
         String argument = reader.readString();
-        if(argument.length() > maxLength) {
+        int length = argument.length();
+        if(length > maxLength) {
             reader.setCursor(position);
-            throw TOO_LONG.createWithContext(reader);
+            throw TOO_LONG.createWithContext(reader, length);
         }
         return argument;
     }
