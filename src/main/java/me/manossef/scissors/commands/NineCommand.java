23b182f29241a61ae2e8f5b0dc951e0069748bc1
ManosSef <mseferlis05@gmail.com>
2026-08-27 18:12:04 +0300

    Update nine command

--- src/main/java/me/manossef/scissors/commands/NineCommand.java
+++ src/main/java/me/manossef/scissors/commands/NineCommand.java
@@ -3,6 +3,7 @@ package me.manossef.scissors.commands;
 import com.mojang.brigadier.CommandDispatcher;
 import me.manossef.scissors.ChatCommandSource;
 import me.manossef.scissors.Commands;
+import me.manossef.scissors.Emojis;
 
 public class NineCommand {
     public static void register(CommandDispatcher<ChatCommandSource> dispatcher) {
@@ -12,7 +13,13 @@ public class NineCommand {
     }
 
     private static int compareNineToEight(ChatCommandSource source) {
-        source.sendSuccess("8 is, in fact, less than 9. You are correct!", false);
+        String prefix = Commands.getPrefix(source.commandMessage().getChannel()).replace(" ", "");
+        if(prefix.equals("8<"))
+            source.sendSuccess("8 is, in fact, less than 9. You are correct!", false);
+        else if(prefix.equals("8>"))
+            source.sendSuccess("No, 8 is not greater than 9. You might need to go back to school", false);
+        else
+            source.sendSuccess("Nooooo you ruined my joke by changing the command prefix " + Emojis.LOUDLY_CRYING_FACE.getFormatted(), false);
         return 1;
     }
 }
